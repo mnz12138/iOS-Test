7b9e9ce8f4aa2743d9454c5caaa8a9b96257e647
王全金 <wangquanjin@rongzi.com>
2017-12-01 16:10:33 +0800

    0.0

---
+++ iOS-Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-Test
+//
+//  Created by 王全金 on 2017/12/1.
+//  Copyright © 2017年 mnz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS-Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-Test
+//
+//  Created by 王全金 on 2017/12/1.
+//  Copyright © 2017年 mnz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
